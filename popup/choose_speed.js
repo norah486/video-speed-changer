1c5fc081297e1111d34ab4922840d765f528ff7c
norah486 <32691116+norah486@users.noreply.github.com>
2021-11-07 21:44:38 -0300

    Implements the more button and more playback rates

--- popup/choose_speed.js
+++ popup/choose_speed.js
@@ -1,15 +1,26 @@
-document.addEventListener("click", function(e) {
+document.addEventListener("click", function(e) {  
   if (e.target.classList.contains("choice")) {
     browser.tabs.executeScript({
       code: `document.querySelectorAll('video').forEach(v => v.playbackRate = ${e.target.textContent})`
     })
   }
 
-  /*else if (e.target.classList.contains("custom")) {
-    browser.tabs.executeScript({
-      file: "/content_scripts/custom_speed.js"
-    })
-  }*/
+  else if (e.target.classList.contains("custom")) {
+
+    var moreBtn = document.getElementById("btnMore");
+    var hidSpeed = document.getElementById("sp_hid");
+    var shownSpeed = document.getElementById("sp_shown");
+
+    if (moreBtn.innerHTML == "More") {
+      moreBtn.innerHTML = "Less"
+      hidSpeed.style.display = "inline"
+      shownSpeed.style.display = "none"
+    } else if (moreBtn.innerHTML == "Less") {
+      moreBtn.innerHTML = "More"
+      hidSpeed.style.display = "none"
+      shownSpeed.style.display = "inline"
+    }
+  }
 
   else {
     return;
